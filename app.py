67fc85329e59cbd79073ff2420ac1894bf78205a
eddieaviles357 <eduardoaviles357@yahoo.com>
2022-12-11 15:10:23 -0800

    fixed typo

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, request, render_template
-from flask_debugtoolbar import DebugToolbarExtension
+# from flask_debugtoolbar import DebugToolbarExtension
 from surveys import Question, Survey, surveys
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "SecretPhrase"
-debug = DebugToolbarExtension(app)
+# debug = DebugToolbarExtension(app)
 # will hold the clients answers
 responses = []
 
