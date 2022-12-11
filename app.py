037b49f4fa5c5ebe2600ed9f3686e52e373cb58c
eddieaviles357 <eduardoaviles357@yahoo.com>
2022-12-11 14:53:29 -0800

    added home route and additional comments

--- app.py
+++ app.py
@@ -4,8 +4,12 @@ from flask_debugtoolbar import DebugToolbarExtension
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "SecretPhrase"
 debug = DebugToolbarExtension(app)
+# will hold the clients answers
+responses = []
 
 
+# home route
 @app.route('/')
 def home():
-    return render_template('base.html')
+    """ Returns a questionaire to the user """
+    return render_template('index.html')
